df8bb105e956f8ac36b350725c0411f9a402ff41
Vera.Zub1 <vera.zub@pm.me>
2019-11-10 17:27:32 +0300

    add new file Hello_ world_with_var.py

---
+++ hello_world_with_var.py
@@ -0,0 +1,27 @@
+#!/usr/lib/python3
+'''
+print my_string
+'''
+
+my_string = "Hello World by var my_string"
+print("my_first_var is "  + my_string)
+
+'''
+print my_string2
+'''
+my_string2 = "Holy Elk by var my_string2"
+print(my_string2)
+
+'''
+print my_strings3. It's a text
+'''
+
+my_strings3 = '''All work and no play makes Jack a dull boy,
+                All work and no play makes Jack a dull boy,
+                All work and no play makes Jack a dull boy
+                '''
+#print(my_strings3)
+
+print("======================================================")
+print( "my_string + my_strings3: " + my_string + " ["+my_strings3+"] ")
+print("======================================================")
