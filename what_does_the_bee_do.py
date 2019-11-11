cfb0f24045080b438455f1f070f72297fb164806
Vera.Zub1 <vera.zub@pm.me>
2019-11-11 22:26:18 +0300

    upd file what_does_the_bee_do.py

--- what_does_the_bee_do.py
+++ what_does_the_bee_do.py
@@ -1,4 +1,4 @@
-#!/usr/lib/python3
+#!/usr/bin/python3
 my_string = '''What does the bee do?
             The bee bring home honey.
             And what does Father do?
@@ -9,32 +9,24 @@ my_string = '''What does the bee do?
             The Baby he Eat up the honey.'''
 print(my_string)
 
+qst1='What does the bee do?'
+ans1=input(qst1)
 
-qst=input('What does the bee do?')
-print(qst+ "- The bee bring home honey")
+qst2='And what does Father do?'
+ans2=input(qst2)
 
-qst=input('And what does Father do?')
-print(qst+ "- The father Bring home money")
+qst3='And what does Mother do?'
+ans3=input(qst3)
 
-qst=input('And what does Mother do?')
-print(qst+ "- The mother Lay out the money.")
+qst4='And what does baby do?'
+ans4=input(qst4)
 
-qst=input ('And what does baby do?')
-print(qst+"- The Baby he Eat up the honey.")
-
-
-
-qst=input("who_said_meow?")
-print(qst)
-
-Q1='Пить будешь?'
-Q2='Пожрешь?'
-A1=input(Q1)
-A2=input(Q2)
-
-print(Q1)
-print(A1)
-print(Q2)
-print(A2)
-
+print(qst1)
+print(ans1)
+print(qst2)
+print(ans2)
+print(qst3)
+print(ans3)
+print(qst4)
+print(ans4)
 
