69224bf81c2941b27b5047c64b5c636079d3bf93
Vera.Zub1 <vera.zub@pm.me>
2019-11-11 20:22:01 +0300

    add file what_does_the_bee_do.py

---
+++ what_does_the_bee_do.py
@@ -0,0 +1,24 @@
+#!/usr/lib/python3
+my_string = '''What does the bee do?
+            The bee bring home honey.
+            And what does Father do?
+            The father Bring home money.
+            And what does Mother do?
+            The mother Lay out the money.
+            And what does baby do?
+            The Baby he Eat up the honey.'''
+print(my_string)
+
+
+qst=input('What does the bee do?')
+print(qst+ "- The bee bring home honey")
+
+qst=input('And what does Father do?')
+print(qst+ "- The father Bring home money")
+
+qst=input('And what does Mother do?')
+print(qst+ "- The mother Lay out the money.")
+
+qst=input ('And what does baby do?')
+print(qst+"- The Baby he Eat up the honey.")
+
